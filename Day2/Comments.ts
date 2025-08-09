eee0cc8d391cf74e0ba7a8bb4b76d84ee81e588c
Codingnaveen46 <naveenreddy44112gmail.com>
2025-08-09 22:13:00 +0530

    first commit

---
+++ Day2/Comments.ts
@@ -0,0 +1,7 @@
+// console.log("Variables in TypeScript");   single line comment  ctrl+/ 
+
+/* console.log("Variables can be declared using let, const, or var.");  multiline comment  for mac : shift +option + A 
+let variable1 = "Hello, World!";
+ */
+
+
---
+++ Day2/Variables.ts
@@ -0,0 +1,3 @@
+//variable : a named storage location in memory or a program that can hold a value. or container which can hold or store data 
+
+// variable declaration 
