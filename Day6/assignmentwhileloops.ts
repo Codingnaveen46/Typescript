84e761f94a38396c4265d8108933f89c0b3cc00a
Codingnaveen46 <naveenreddy44112gmail.com>
2025-08-11 21:32:05 +0530

    learnt half loops till while loop

---
+++ Day6/assignmentwhileloops.ts
@@ -0,0 +1,13 @@
+//while loop 
+
+//1 Write a program to calculate the sum of the first 10 natural numbers using a while loop. 
+
+let sum:number= 0;
+let count:number = 1; 
+
+while(count <= 10) {
+    sum += count; // sum = sum + count
+    count++;
+}
+console.log("The sum of the first 10 natural numbers is: " + sum);
+
---
+++ Day6/whileloop.ts
@@ -0,0 +1,64 @@
+//  while loop: a while loop executes as long as the condition is true 
+
+
+/* 
+
+while (condition)
+{
+
+statements : 
+
+}
+*/
+
+//example 1 
+
+/* let num1:number = 1;     //stopping point 10  each time we need increase 1 
+
+while(num1 <= 10){
+    console.log(num1);
+    num1++;
+} */
+
+
+
+//example 2 
+
+//print even number  1 to 10 
+
+
+
+//method 1 
+
+/* let num2:number = 2;
+
+while(num2 <= 10){
+    console.log(num2);
+    num2 +=2;
+    
+} */
+
+//method 2 
+/* 
+let num3:number = 1;
+while(num3 <= 10){
+    if(num3 % 2 === 0){
+        //if the number is even
+        //even number
+
+    console.log(num3);
+    }
+    num3++;
+} */
+
+
+//example 3  print 10 to 1 from decending 
+
+/* let num4:number = 10;
+while(num4 >= 1){
+    console.log(num4);
+    num4--;
+} */
+
+// if the condition never becomes false, the loop will run forever. 
+
